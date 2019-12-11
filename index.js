d37602e89f32cd86647b750141a433bd203b89ba
deanelut <deanelut@gmail.com>
2019-12-11 13:55:51 -0700

    tests

--- index.js
+++ index.js
@@ -97,16 +97,16 @@ function replaceUrls(text) {
 	}
 }
 
-Typer.speed=3;
-Typer.file="CodeNerve.txt";
-Typer.init();
+// Typer.speed=3;
+// Typer.file="CodeNerve.txt";
+// Typer.init();
  
-var timer = setInterval("t();", 30);
-function t() {
-	Typer.addText({"keyCode": 123748});
+// var timer = setInterval("t();", 30);
+// function t() {
+// 	Typer.addText({"keyCode": 123748});
 	
-	if (Typer.index > Typer.text.length) {
-		clearInterval(timer);
-	}
-}
+// 	if (Typer.index > Typer.text.length) {
+// 		clearInterval(timer);
+// 	}
+// }
  
